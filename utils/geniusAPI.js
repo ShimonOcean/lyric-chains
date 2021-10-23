e23f16f95e4393b868c83a4d65b51db72ad796ce
ShimonIslam <shimon.ocean@gmail.com>
2021-10-22 17:47:37 -0700

    geniusArtistTopTen, needs tests

--- utils/geniusAPI.js
+++ utils/geniusAPI.js
@@ -1,9 +1,22 @@
 const axios = require('axios');
 
-const geniusArtistTopTen = (artist) => {
 
+const getURLResponse = (url) => {
+    const axiosResp = axios.get(url);
+    return axiosResp;
+}
+
+const geniusArtistTopTen = (artist) => {
+    return new Promise((res, rej) => {
+        getURLResponse(`api.genius.com/search?q=${Drake}`)
+            .then((response) => {
+                var result = response.data.response.sections[0].hits[0].result;
+                console.log(result)
+            })
+    })
 }
 
 module.exports = {
     getArtistTopTen,
 }
+
