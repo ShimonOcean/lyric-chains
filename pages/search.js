a7784e06b4fffa20bb67d6bfe126971b5797691d
ShimonIslam <shimon.ocean@gmail.com>
2021-11-04 21:05:35 -0700

    Data Fetching, getServerSideProps in search.js, need to pull search term from query url

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
+const client_id = process.env.GENIUS_CLIENT_ID;
+const redirect_uri = "https://httpbin.org/anything";
+
 
 function Search() {
     return <div>
@@ -15,7 +18,27 @@ function Search() {
 
 export default Search
 
-// export async function getServerSideProps(context) {
-//     const useDummyData = false;
+export async function getServerSideProps() {
+  let params = {
+    client_id: client_id,
+    redirect_uri: redirect_uri,
+    response_type: "code",
+    scope: "me",
+  };
 
-// }
+  let endpoint = "https://api.genius.com/oauth/authorize?"; 
+  endpoint = endpoint + new URLSearchParams(params);
+  const res = await fetch(`endpoint`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
