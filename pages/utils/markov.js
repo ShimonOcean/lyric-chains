7eae250ce8e5c6e364a32a993094c3add4b7fe74
ShimonIslam <shimon.ocean@gmail.com>
2021-11-10 12:41:34 -0800

    Markov, setting up prediction functions + randInt

--- pages/utils/markov.js
+++ pages/utils/markov.js
@@ -13,6 +13,21 @@ class MarkovChain {
         nextDict[next] = nextWord + 1
         this.chain[prev] = nextDict
     }
+
+    predictor(prev) {
+        if (Object.keys(this.chain).length == 0){
+            throw new Error('No elements in Markov Chain')
+        }
+        const nextDict = this.chain[prev]
+        if (nextDict == undefined) {
+            const randIdx = randInt(Object.keys(this.chain).length)
+            return Object.keys(this.chain)[randIdx]
+        }
+    }
+}
+
+const randInt = (max) => {
+    return Math.floor(Math.random() * max);
 }
 
 const rawTextToWords = (text) => {
